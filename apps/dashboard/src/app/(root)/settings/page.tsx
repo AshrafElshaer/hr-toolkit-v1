58b53353c4b2bbf2bf234fe1a542496e96b04cde
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-09-16 12:14:17 -0500

    add settings page

---
+++ apps/dashboard/src/app/(root)/settings/page.tsx
@@ -0,0 +1,11 @@
+import Main from "@/components/main";
+
+export default function SettingsPage() {
+  return (
+    <Main>
+      <div>
+        <h1>Settings</h1>
+      </div>
+    </Main>
+  );
+}
