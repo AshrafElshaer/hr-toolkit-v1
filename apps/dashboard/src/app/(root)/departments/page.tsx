2c3bf04e1becd61fb9ad915cc2d538fe1c2232ae
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-10-30 11:37:25 -0500

    redirect none admin

--- apps/dashboard/src/app/(root)/departments/page.tsx
+++ apps/dashboard/src/app/(root)/departments/page.tsx
@@ -8,6 +8,9 @@ import {
 } from "@/features/departments/components/table/columns";
 import { DepartmentsTable } from "@/features/departments/components/table/table";
 import { departmentsTableFiltersSearchParamsCache } from "@/features/departments/lib/departments-table-params";
+import { createServerClient } from "@/lib/supabase/server";
+import { getUserDepartment } from "@toolkit/supabase/queries";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Departments",
@@ -20,6 +23,20 @@ export default async function DepartmentsListPage({
   searchParams,
 }: DepartmentsListPageProps) {
   const params = departmentsTableFiltersSearchParamsCache.parse(searchParams);
+
+  const supabase = createServerClient();
+  const { data: user } = await supabase.auth.getUser();
+  const userRole = user?.user?.user_metadata?.role;
+
+  if (userRole !== "admin") {
+    const { data: userDepartmentData } = await getUserDepartment(
+      supabase,
+      user.user?.id ?? "",
+    );
+    const userDepartment = userDepartmentData?.department;
+    if (!userDepartment) redirect("/");
+    return redirect(`/departments/${userDepartment.id}`);
+  }
 
   const result = await getDepartmentsAction({ ...params });
   const departments = result?.data;
